e86635547bcf642cb34f3f267a8d26bde83eef04
jafeershaik5 <jafeershaik5@gmail.com>
2024-09-16 11:17:00 +0530

    fallback to render

--- server/index.js
+++ server/index.js
@@ -146,12 +146,7 @@ app.get('/xdl', async (req, res) => {
 
 
 });
-module.exports = app;
 
-// // Start the server for local development
-// if (process.env.NODE_ENV !== 'production') {
-//     const port = process.env.PORT || 4000;
-//     app.listen(port, () => {
-//         console.log(`Server is running on PORT: ${port}`);
-//     });
-// }
+app.listen(port, () => {
+    console.log(`Server is running on PORT: ${port}`);
+});
